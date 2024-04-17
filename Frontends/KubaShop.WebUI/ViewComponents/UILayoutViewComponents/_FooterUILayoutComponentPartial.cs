9fea6dfb29b62949de58b83217a4667c0aabb4e8
kubrakaradirek <karadirekkubra96@gmail.com>
2024-04-17 19:59:07 +0300

    Kullanıcı Arayüzü Layout Tasarlandı

---
+++ Frontends/KubaShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KubaShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
