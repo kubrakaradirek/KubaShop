a29ce2c4573f4300a6165c788e6c04f9de1b148d
kubrakaradirek <karadirekkubra96@gmail.com>
2024-04-17 17:20:26 +0300

    sa

---
+++ Frontends/KubaShop.WebUI/Controllers/DafaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KubaShop.WebUI.Controllers
+{
+    public class DafaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/KubaShop.WebUI/ViewComponents/UILayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KubaShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _HeadUILayoutComponentPartial:ViewComponent
+    {
+        //View componentin amacı sayfaları partiallara ayırmaktır.
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
