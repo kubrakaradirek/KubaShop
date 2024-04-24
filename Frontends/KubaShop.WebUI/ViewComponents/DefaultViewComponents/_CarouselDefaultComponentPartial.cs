49c132db9e41a8209e84072e81675033d0d5842f
kubrakaradirek <karadirekkubra96@gmail.com>
2024-04-24 15:01:15 +0300

    Special Offer Tablosu ve MongoDb den önceki güncelleme

--- Frontends/KubaShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
+++ Frontends/KubaShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KubaShop.DtoLayer.CatalogDtos.FeatureSliderDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace KubaShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _CarouselDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+      
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CarouselDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:9696/api/FeatureSliders");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
+
     }
 }
